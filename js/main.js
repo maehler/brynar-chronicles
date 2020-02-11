66cc5ff89036da95f7af8ff0c718fc5db0b27abf
Niklas Mähler <niklas.mahler@gmail.com>
2020-02-11 23:15:59 +0100

    Remove console.log

--- js/main.js
+++ js/main.js
@@ -7,7 +7,6 @@ for (let i = 0; i < entries.length; ++i) {
     let entryLink = document.createElement('a');
     entryLink.appendChild(document.createTextNode(title));
     navElement.appendChild(entryLink);
-    console.log(navElement);
     nav.appendChild(navElement);
 }
 
