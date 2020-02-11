a38677e61fb234f2fd4345fd32918d95333aa0c0
Niklas Mähler <niklas.mahler@gmail.com>
2020-02-11 22:32:45 +0100

    Convert markdown to HTML and insert onto page

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,11 @@
 const browsersync = require('browser-sync').create();
 const cleanCSS = require('gulp-clean-css');
 const gulp = require('gulp');
+const pandoc = require('gulp-pandoc');
 const rename = require('gulp-rename');
 const sass = require('gulp-sass');
+const concat = require('gulp-concatenate').default;
+const inject = require('gulp-inject');
 
 function browserSync(callback) {
     browsersync.init({
@@ -34,11 +37,38 @@ function watchFiles() {
     gulp.watch('**/*.html', reload);
 }
 
-gulp.task('serve', function() {
-    return gulp.src('')
-});
+function markdown() {
+    return gulp.src('entries/md/*.md')
+        .pipe(pandoc({
+            from: 'markdown',
+            to: 'html5',
+            ext: '.html',
+            args: [
+                '--section-divs',
+                '--standalone',
+                '--template=templates/entry_template.html'
+            ]
+        }))
+        .pipe(concat('all.html'))
+        .pipe(gulp.dest('entries/'));
+}
 
+function index() {
+    return gulp.src('index.html')
+        .pipe(inject(gulp.src(['entries/all.html']), {
+            starttag: '<!-- inject:entries -->',
+            relative: true,
+            transform: function(filePath, file) {
+                return file.contents.toString('utf8');
+            }}))
+        .pipe(gulp.dest('./'));
+}
+
+const build = gulp.parallel(css, markdown, index);
 const watch = gulp.parallel(watchFiles, browserSync);
 
+exports.build = build;
 exports.css = css;
+exports.markdown = markdown;
 exports.watch = watch;
+exports.index = index;
